77c692c186a034b1938a9769b4475df22963f16c
Gesugao-san <gesugao.san@gmail.com>
2022-02-25 20:13:29 +0300

    Cache file format changed

--- code/url_gen.js
+++ code/url_gen.js
@@ -8,19 +8,38 @@ to use for representing numeric values. In this case, iterating from 10 to 35, c
 every number to a string using 36 as the base will return the alphabet's character that
 we'll concatenate into a string: */
 
-let alphabet1 = '0', alphabet2 = '0', out = [];
+//let alphabet1 = '0', alphabet2 = '0', out = [];
+let alph1, alph2, alph3, alph4, out = [];
 
-for(i = -1; ++i < 16;) {
-	alphabet1 = i.toString(36).toLowerCase();
+/* for(i = -1; ++i < 16;) {
+	alphabet1 = i.toString(36).toLowerCase(); // a-f
 	for(ii = -1; ++ii < 16;) {
-		alphabet2 = ii.toString(36).toLowerCase();
-		out.push(alphabet1 + alphabet2);
+		alphabet2 = ii.toString(36).toLowerCase(); // a-f
+		out.push(alphabet1 + alphabet2); // '00' - 'ff'
+	}
+} */
+for (i1 = 9; ++i1 < 36;) { // a-z
+	alph1 = i1.toString(36).toLowerCase();
+	for(i2 = 9; ++i2 < 36;) { // a-z
+		alph2 = i2.toString(36).toLowerCase();
+		for(i3 = -1; ++i3 < 10;) { // 0-9
+			alph3 = i3.toString(36);
+			for(i4 = -1; ++i4 < 10;) { // 0-9
+				alph4 = i4.toString(36);
+				//id++; // '0001' - '3600'
+				//out.push([String(id).padStart(4, '0'), alph1 + alph2 + alph3 + alph4, "?"]); // aa00 - ff99
+				//out.push([alph1 + alph2 + alph3 + alph4, "?"]); // aa00 - ff99
+				out.push(JSON.parse('{"id":"' + alph1 + alph2 + alph3 + alph4 + '","data":"?"}'));
+			}
+		}
 	}
 }
 
+out.unshift(JSON.parse('{"time":"' + new Date().toISOString() + '"}'));
 // https://stackoverflow.com/a/5915122/8175291
 var random_item = out[Math.floor(Math.random()*out.length)];
 
-console.log(out.slice(0, 6), "...");
-console.log(out.slice(-6, out.length));
-console.log("Random URL: http://aa.mail.ru/promo/" + random_item + "/");
+console.clear();
+console.log(out.slice(0, 4), "...");
+console.log(out.slice(-3, out.length));
+console.log("Random URL: http://aa.mail.ru/promo/" + random_item.id + "/");
--- index.js
+++ index.js
@@ -2,18 +2,20 @@
 const request = require('request');
 const fs = require('fs');
 
-var out = [], RedirectCodes = [];
+var verbose = 1, rescan = 0;
 var path = './cache/result.json';
 var template = [ "https://archeage.ru/promo/", "/index.html"];
 
-function indexOfArray2D(array, target) {
+var out = [], RedirectCodes = [];
+
+/* function indexOfArray2D(array, target) {
 	for (var i = 0; i < array.length; i++) {
 		if (array[i][1] == target) {
 			return i;
 		}
 	}
 	return false;
-}
+} */
 
 // populate
 function generateArray() {
@@ -28,22 +30,23 @@ function generateArray() {
 		http://aa.mail.ru/subscribe, http://aa.mail.ru/promo/advent/.
 		//
 	*/
-	for(i1 = 9; ++i1 < 16;) {
-		alph1 = i1.toString(36).toLowerCase(); // a-z
-		for(i2 = 9; ++i2 < 16;) {
-			alph2 = i2.toString(36).toLowerCase(); // a-z
-			for(i3 = -1; ++i3 < 10;) {
-				alph3 = i3.toString(36); // 0-9
-				for(i4 = -1; ++i4 < 10;) {
-					alph4 = i4.toString(36); // 0-9
+	for(i1 = 9; ++i1 < 36;) { // a-z
+		alph1 = i1.toString(36).toLowerCase();
+		for(i2 = 9; ++i2 < 36;) { // a-z
+			alph2 = i2.toString(36).toLowerCase();
+			for(i3 = -1; ++i3 < 10;) { // 0-9
+				alph3 = i3.toString(36);
+				for(i4 = -1; ++i4 < 10;) { // 0-9
+					alph4 = i4.toString(36);
 					//id++; // '0001' - '3600'
-					//out.push([String(id).padStart(4, '0'), alph1 + alph2 + alph3 + alph4, "?"]); // aa00 - ff99
-					out.push([alph1 + alph2 + alph3 + alph4, "?"]); // aa00 - ff99
+					//out.push([String(id).padStart(4, '0'), alph1 + alph2 + alph3 + alph4, "?"]); // aa00 - zz99
+					//out.push([alph1 + alph2 + alph3 + alph4, "?"]); // aa00 - zz99
+					out.push(JSON.parse('{"id":"' + alph1 + alph2 + alph3 + alph4 + '","data":"?"}'));
 				}
 			}
 		}
 	}
-	out.unshift(new Date().toISOString());
+	out.unshift(JSON.parse('{"time":"' + new Date().toISOString() + '"}'));
 	for (var i = 300; i <= 399; i++) {
 		RedirectCodes.push(i);
 	}
@@ -54,9 +57,15 @@ function writeCache(path, data) {
 	if (!fs.existsSync(path)) {
 		console.log('File not found! Rewriting...');
 	}
-	if (Array.isArray(data[0])) {
-		data.unshift(new Date().toISOString());
+	if (!data[0].hasOwnProperty('time')) {
+		data.unshift(JSON.parse('{"time":"' + new Date().toISOString() + '"}'));
 	}
+	/* let dataToWrite = [];
+	for (var i = 1; i < data.length; i++) {
+		dataToWrite.push(JSON.stringify(data.id, data.state));
+		console.log("dataToWrite:", dataToWrite[0]);
+		break;
+	} */
 	// https://stackoverflow.com/a/31777314/8175291
 	fs.writeFileSync(path, JSON.stringify(data, null, 4), { flag: 'w' }, function(error) { // 'wx' for "EEXIST"
 		if (error) {
@@ -67,22 +76,22 @@ function writeCache(path, data) {
 	});
 }
 
-function doRequest(targetIndex) {
+function doRequest(i) {
 	return new Promise((resolve, reject) => {
 		// request('https://bitskins.com/api/v1/get_account_balance/?api_key='+api+'&code='+code, function (error, response, body) {
 		request(req_options, function (error, response, body) {
 			// in addition to parsing the value, deal with possible errors
 			if (response.statusCode == 200) {
 				process.stdout.write('200 OK — ');
-				out[targetIndex][1] = '~';
+				out[i].data = '~';
 			}
 			if (response.statusCode in RedirectCodes) { // Array.from(Array(300, 399).keys())
 				//console.log('Redirect (status code):', response.statusCode);
-				out[targetIndex][1] = '-';
+				out[i].data = '-';
 				resolve('redirect (status code):', response.statusCode); // https://stackoverflow.com/a/48839845/8175291
 			} else if (response.request.uri.href == "https://archeage.ru/" || response.request.uri.href != req_options.uri ) { // https://stackoverflow.com/a/17362976/8175291
 				//console.log('Redirect (request href) — there is no promo');
-				out[targetIndex][1] = '-';
+				out[i].data = '-';
 				resolve("redirect (request href), there is no promo"); // https://stackoverflow.com/a/48839845/8175291
 			} else if (response.statusCode != 200) {
 				console.log('Error:', response.statusCode);
@@ -105,29 +114,43 @@ async function mainLoop() {
 	} catch (error) {
 		console.error("Error on read cache file:\n", error);
 	}
-	var out_length = out.length - 3595;
+	var out_length = out.length;// - 3595;
 	console.log("Cache file loaded, last update time:", out[0]);
 	out.shift();
+
 	//console.log("out:", out[0]);
-	console.log("Template URL:", template[0] + "__DATA__" + template[1] + ", Data: [aa00-ff99]");
-	console.log("Total promo to check:", out_length);
+	console.log("Template URL:", template[0] + "__DATA__" + template[1] + ", Data: [" + out[0].id + "-" + out[out_length-2].id + "]");
+	console.log("Total promo to check: " + out_length + "\nStart cache veryfing and get missing (verbose: " + (verbose ? "true" : "false") + ")...");
 	for (var i = 1; i < out_length; i++) {
-		let targetIndex = indexOfArray2D(out, out[i][1]);
-		let state = out[targetIndex][1];
-		req_options.uri = template[0] + out[i][1] + template[1];
+		//let targetIndex = indexOfArray2D(out, out[i].data);
+		let id = out[i].id, state = out[i].data;
+		req_options.uri = template[0] + id + template[1];
 		//console.log('Data receiving in progress...', req_options.uri);
-		process.stdout.write('Target [' + ('000' + (i)).slice(-out_length)  + '\\' + out_length + ']: ' + out[i][0] + ' — ');
-		if (state == "?") { //in ["?", "~"]
-			await doRequest(targetIndex).then(function(val) {
+		if (verbose) process.stdout.write('Target [' + ('000' + (i)).slice(-out_length)  + '\\' + out_length + ']: ' + id + ' — ');
+		if ((state == "?") || rescan) { //in ["?", "~"]
+			await doRequest(i).then(function(val) {
 				console.log(val);
 			}).catch(function(err) {
 				console.error(err);
 			});
 		} else {
-			console.log("skipping due to already resolved: \"" + state + "\"");
+			if (verbose) console.log("skipping due to already resolved: \"" + state + "\"");
 		}
 		writeCache(path, out, true);
 	}
+
+	console.log("Cache veryfing and getting missing is done, analyzing...");
+	for (var i = 1; i < out_length; i++) {
+		//let targetIndex = indexOfArray2D(out, out[i].data);
+		let state = out[i].data, id = out[i].id;
+		req_options.uri = template[0] + id + template[1];
+		//console.log('Data receiving in progress...', req_options.uri);
+		if (state == "+") {
+			console.log('Target [' + ('000' + (i)).slice(-out_length)  + '\\' + out_length + ']: ' + id + ' — ' + req_options.uri);
+		}
+		writeCache(path, out, true);
+	}
+	console.log("Analyzing done.");
 }
 
 
